6077a2d85732e0437daeccc50bec356827bf1414
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2017-11-19 11:34:54 -0500

    added picasso and image display

--- app/src/main/java/io/github/vanessa85/candycode/DetailActivity.java
+++ app/src/main/java/io/github/vanessa85/candycode/DetailActivity.java
@@ -3,7 +3,10 @@ package io.github.vanessa85.candycode;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String CANDY = "candy";
@@ -16,6 +19,7 @@ public class DetailActivity extends AppCompatActivity {
         TextView tvName = findViewById(R.id.tvName);
         TextView tvPrice = findViewById(R.id.tvPrice);
         TextView tvDescription = findViewById(R.id.tvDescription);
+        ImageView imageView = findViewById(R.id.imageView);
 
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(CANDY)) {
@@ -24,10 +28,9 @@ public class DetailActivity extends AppCompatActivity {
             tvName.setText(candy.name);
             tvPrice.setText(candy.price);
             tvDescription.setText(candy.description);
+
+            Picasso.with(this).load(candy.image).into(imageView);
         }
-
-
-
 
     }
 }
