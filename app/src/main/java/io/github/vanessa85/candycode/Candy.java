643c749389c4fdaf7c8d1906ce1294a1b4c934fc
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2017-11-19 10:40:51 -0500

    using Android Async Http for get list candies

---
+++ app/src/main/java/io/github/vanessa85/candycode/Candy.java
@@ -0,0 +1,15 @@
+package io.github.vanessa85.candycode;
+
+/**
+ * Created by vanessa on 11/19/17.
+ */
+
+public class Candy {
+    public int id;
+    public String name;
+    public String image;
+    public String description;
+    public String price;
+
+
+}
--- app/src/main/java/io/github/vanessa85/candycode/MainActivity.java
+++ app/src/main/java/io/github/vanessa85/candycode/MainActivity.java
@@ -3,13 +3,21 @@ package io.github.vanessa85.candycode;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+
 import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,17 +31,8 @@ public class MainActivity extends AppCompatActivity {
 
         final ArrayList<String> candyList = new ArrayList<String>();
         candyList.add("Tropical Wave");
-        candyList.add("Berry Bouncer");
-        candyList.add("Grape Gummer");
-        candyList.add("Apple of My Eye");
-        candyList.add("Much Minty");
-        candyList.add("So Fresh");
-        candyList.add("Sassy Sandwich Cookie");
-        candyList.add("Uni-pop");
-        candyList.add("Straberry Surprise");
-        candyList.add("Wish Upon a Start");
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 R.layout.list_item_candy,
                 R.id.tvCandy,
                 candyList);
@@ -46,6 +45,26 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this, DetailActivity.class);
                 intent.putExtra(DetailActivity.CANDY_NAME, candyList.get(i));
                 startActivity(intent);
+            }
+        });
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://vast-brushlands-23089.herokuapp.com/main/api", new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.e("AsyncHttpClient", responseString);
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                Log.d("AsyncHttpClient", responseString);
+
+                Gson gson = new GsonBuilder().create();
+                Candy[] candies = gson.fromJson(responseString, Candy[].class);
+                adapter.clear();
+                for (Candy candy : candies) {
+                    adapter.add(candy.name);
+                }
             }
         });
 
