582836e9ab279cc5e675b9d829fbc4983140c042
Vanessa M. Paco Ramos <vanessapr.85@gmail.com>
2017-11-19 08:12:33 -0500

    init project

---
+++ app/src/main/java/io/github/vanessa85/candycode/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.vanessa85.candycode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
